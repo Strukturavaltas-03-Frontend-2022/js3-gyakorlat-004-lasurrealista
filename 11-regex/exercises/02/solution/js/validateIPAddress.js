c55988adebb91acf240882661bda8e8b1d9ea8eb
Evelin <ladyblue@freemail.hu>
2022-08-29 16:54:01 +0200

    solution

--- 11-regex/exercises/02/solution/js/validateIPAddress.js
+++ 11-regex/exercises/02/solution/js/validateIPAddress.js
@@ -1,1 +1,7 @@
-export default validateIPAddress;
+const validateIPAddress = (ipAddress) => {
+    const IPPattern = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|$)){4}$/;
+    return IPPattern.test(ipAddress);
+  };
+  
+  export default validateIPAddress;
+  
